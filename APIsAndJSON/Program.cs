910ae2334b92e1041411e3266c653acb4ff7b0d1
Alex Leifermann <aleif92@outlook.com>
2024-08-13 14:26:23 -0500

    finished

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace APIsAndJSON
+﻿using System.Text.Json.Nodes;
+using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+            RonVSKanyeAPI.Convo();
+
+            
+          
+
+
+          
+           
         }
     }
 }
--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json.Nodes;
+using Newtonsoft.Json.Linq;
 
 namespace APIsAndJSON
 {
     internal class RonVSKanyeAPI
     {
+
+        public static void Convo()
+        {
+            var client = new HttpClient();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Kanye says:\n {GetKenyeQoute(client)}\n");
+                Thread.Sleep(2000);
+                Console.WriteLine($"Ron says:\n{GetRonQuote(client)}\n");
+            }
+        }
+
+        private static string GetKenyeQoute(HttpClient client)
+        {
+            
+            var KanyeResponse = client.GetStringAsync("https://api.kanye.rest/").Result;
+
+            return JObject.Parse(KanyeResponse).GetValue("quote").ToString();
+        }
+
+        private static string GetRonQuote(HttpClient client)
+        {
+            var RonResponse = client.GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes").Result;
+            return RonResponse.Replace('[', ' ').Replace(']', ' ').Replace('"', ' ').Trim();
+        }
+
+
+
+
     }
 }
