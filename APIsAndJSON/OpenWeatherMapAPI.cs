0baa4fd5ad5a89a9f6e851bff166263113c236cd
Alex Leifermann <aleif92@outlook.com>
2024-08-14 07:42:38 -0500

    deleted api key

--- APIsAndJSON/OpenWeatherMapAPI.cs
+++ APIsAndJSON/OpenWeatherMapAPI.cs
@@ -9,7 +9,7 @@ namespace APIsAndJSON
 {
     internal class OpenWeatherMapAPI
     {
-        //https://api.openweathermap.org/data/2.5/weather?zip=55374&appid=1fa22226a62c7c27875ba273b030168d&units=imperial
+        
         
         public static void GetTemp()
         {
@@ -18,21 +18,29 @@ namespace APIsAndJSON
         
             //Turn the json into an object to grab the piece I need (api key)
             var apiKey = JObject.Parse(appsettingsText).GetValue("key").ToString();
+            
+            
             //Ask user for there Zip
             Console.WriteLine("Enter Zip Code:");
+            
+            
             //Enter the zip code to provide for the url
             var zip = Console.ReadLine();
             
-            //buil the url from the api call comes from 
+            
+            //build the url (weather api endpoint)
             var url = $"https://api.openweathermap.org/data/2.5/weather?zip={zip}&appid={apiKey}&units=imperial";
             
             //Create an HTTPClient instance to make the api call
             var client = new HttpClient();
+            
 
             var jsonText = client.GetStringAsync(url).Result;
             
+            
             //Parse the string as a json Object - this obj can be indexed like an array
             var weatherObj = JObject.Parse(jsonText);
+            
             
             //Print the info we need using the weaterh obj
             Console.WriteLine($"Temp: {weatherObj["main"]["temp"]}");
